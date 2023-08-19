d5b76abeba89e37f52dff07c877aa5ae3abea72d
gwgarcia21 <gwgarcia21@gmail.com>
2023-08-19 15:52:51 -0300

    organização do layout do loudness meter

--- AvaloniaTutorial/MainWindow.axaml.cs
+++ AvaloniaTutorial/MainWindow.axaml.cs
@@ -11,29 +11,5 @@ namespace AvaloniaTutorial
         {
             InitializeComponent();
         }
-
-        public void ButtonClicked(object source, RoutedEventArgs args)
-        {
-            Calculate();
-        }
-
-        public void TextChanged(object source, RoutedEventArgs args)
-        {
-            Calculate();
-        }
-
-        private void Calculate()
-        {
-            if (Double.TryParse(celsius.Text, out double C))
-            {
-                var F = C * (9d / 5d) + 32;
-                fahrenheit.Text = F.ToString("0.0");
-            }
-            else
-            {
-                celsius.Text = "0";
-                fahrenheit.Text = "0";
-            }
-        }
     }
 }
